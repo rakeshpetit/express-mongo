55d2d5bddcf0d5366388b6125680456a13ae4883
Rakesh <rakeshpetit@gmail.com>
2019-09-05 16:29:31 +0100

    delete todos route

--- server.js
+++ server.js
@@ -44,6 +44,21 @@ app.get('/todos/:id', (req, res) => {
     });
 })
 
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send()
+    }
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send()
+        }
+        res.send({todo})
+    }).catch((err) => {
+        res.status(400).send(err)
+    });
+})
+
 app.listen(3000, () => {
     console.log('Server on port 3000')
 })
