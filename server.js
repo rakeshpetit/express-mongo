a9bd7e8e8ff5ce5f9eb82aa277e8ba76ed42327b
Rakesh <rakeshpetit@gmail.com>
2019-09-13 15:28:16 +0100

    get patch delete routes private

--- server.js
+++ server.js
@@ -37,12 +37,12 @@ app.get("/todos", authenticate, (req, res) => {
         });
 });
 
-app.get("/todos/:id", (req, res) => {
+app.get("/todos/:id", authenticate, (req, res) => {
     const id = req.params.id;
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
-    Todo.findById(id)
+    Todo.findOne({ _id: id, createdBy: req.user._id })
         .then(todo => {
             if (!todo) {
                 return res.status(404).send();
@@ -54,12 +54,12 @@ app.get("/todos/:id", (req, res) => {
         });
 });
 
-app.delete("/todos/:id", (req, res) => {
+app.delete("/todos/:id", authenticate, (req, res) => {
     const id = req.params.id;
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
-    Todo.findByIdAndRemove(id)
+    Todo.findOneAndRemove({ _id: id, createdBy: req.user._id })
         .then(todo => {
             if (!todo) {
                 return res.status(404).send();
@@ -71,7 +71,7 @@ app.delete("/todos/:id", (req, res) => {
         });
 });
 
-app.patch("/todos/:id", (req, res) => {
+app.patch("/todos/:id", authenticate, (req, res) => {
     const id = req.params.id;
     const { text } = req.body;
     let { completed } = req.body;
@@ -84,8 +84,8 @@ app.patch("/todos/:id", (req, res) => {
     } else {
         completed = false;
     }
-    Todo.findByIdAndUpdate(
-        id,
+    Todo.findOneAndUpdate(
+        { _id: id, createdBy: req.user._id },
         { $set: { text, completed, completedAt } },
         { new: true }
     )
